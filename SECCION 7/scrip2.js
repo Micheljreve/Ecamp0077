40c659afc7f6ee1b5f157a03467b91b939b846ea
Micheljreve <michelretamalvega@gmail.com>
2024-07-19 12:42:34 -0400

    .

---
+++ SECCION 7/scrip2.js
@@ -0,0 +1,54 @@
+
+/* console.log("Hola desde la consola JS");*/
+document.body
+alert("Bienvenido a la calculadora Basica");
+console.log("Bienvenido a la calculador Basica");
+alert("Opciones: 1. Suma, 2. Resta , 3. Multiplicacion, 4. Division 5.IVA");
+opcion = parseInt(prompt("Escribe el numero de la opcion deseada"));
+var a;
+var b;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
